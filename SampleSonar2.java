13c40782423e8f44b29c158a83498b7c4c64d8e3
vikiarora <52859016+vikiarora@users.noreply.github.com>
2020-03-29 03:11:34 +0100

    Create SampleSonar2.java

---
+++ SampleSonar2.java
@@ -0,0 +1,30 @@
+public class SampleSonar2 {
+	private int abc = 66;
+	
+	public int getAbc() {
+		return abc;
+	}
+
+	public void setAbc(int abc) {
+		this.abc = abc;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SampleSonar2 ss = new SampleSonar2();
+		int ppp = 444;
+		System.out.println("abc /5" + ss.getAbc()/5);
+		for (int i=0;i<4;i++) {
+			for (int k=0;k<4;k++) {
+				for (int m=0;m<4;m++) {
+					for (int m2=0;m2<4;m2++) {
+						System.out.println("HelloE");
+					}
+				}
+				
+			}
+			
+		}
+	}
+
+}

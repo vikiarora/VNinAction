cef6f7fb591f044a06cf34739159929057d62a7f
VishalNagpal <VishalNagpal@LAPTOP-EMHLJNGR>
2020-03-29 02:14:40 +0100

    First commit

---
+++ SonarCloud/SonarCloudTest/src/com/ng/sonarqube/SampleSonar.java
@@ -0,0 +1,32 @@
+package com.ng.sonarqube;
+
+public class SampleSonar {
+	private int abc = 66;
+	
+	public int getAbc() {
+		return abc;
+	}
+
+	public void setAbc(int abc) {
+		this.abc = abc;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SampleSonar ss = new SampleSonar();
+		int ppp = 444;
+		System.out.println("abc /5" + ss.getAbc()/5);
+		for (int i=0;i<4;i++) {
+			for (int k=0;k<4;k++) {
+				for (int m=0;m<4;m++) {
+					for (int m2=0;m2<4;m2++) {
+						System.out.println("HelloE");
+					}
+				}
+				
+			}
+			
+		}
+	}
+
+}

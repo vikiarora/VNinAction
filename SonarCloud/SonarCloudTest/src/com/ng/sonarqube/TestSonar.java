e7cdb9a344b58c65aaba8ba0c19835113fce0e83
VishalNagpal <VishalNagpal@LAPTOP-EMHLJNGR>
2020-03-29 02:26:44 +0100

    sample file commit for a push

---
+++ SonarCloud/SonarCloudTest/src/com/ng/sonarqube/TestSonar.java
@@ -0,0 +1,10 @@
+package com.ng.sonarqube;
+
+public class TestSonar {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Sample file created for a commit");
+	}
+
+}
